3a89d4b3b43083a5d814cc28e5247ff301110cbb
Bárbara Hellen <barbarahellen993@gmail.com>
2023-07-06 08:39:36 -0300

    adicionando d016

--- desafios/d016.py
+++ desafios/d016.py
@@ -1,3 +1,10 @@
-# crie um programa que leia umm número real qualquer
-# pelo teclado e mostre na tel asua porção inteira
-# exemplo: numero 6.127 tem e porção inteira 6
+''' crie um programa que leia umm número real qualquer
+    pelo teclado e mostre na tela sua porção inteira
+    exemplo: numero 6.127 tem e porção inteira 6     '''
+
+from math import trunc
+
+numero = float(input("digite um número: "))
+
+# trunc apaga o que tem depois da vírgula
+print(f"porção inteira = {trunc(numero)}")
